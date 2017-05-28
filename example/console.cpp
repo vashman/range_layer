0476ff8f45aba2f567ef31945ce614c186a8de8d
Sunny S <3030712@gmail.com>
2017-05-28 03:10:42 -0700

    added output example.

--- example/console.cpp
+++ example/console.cpp
@@ -6,17 +6,27 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 #include <cassert>
+#include <array>
+#include "../include/array_range.hpp"
 #include "../include/console_range.hpp"
 #include "../include/range.hpp"
 #include "../include/algorithm.hpp"
 
+using std::array;
+using range_layer::array_range;
 using range_layer::input_console_range;
 using range_layer::output_console_range;
 
 int main (){
 
-auto in = input_console_range();
+range_layer::execution_policy::sequenced seq{};
+char arr[] = "Press a button and press enter to continue.";
+array_range<char> rng {arr, arr + 43};
+
 auto out = output_console_range();
+write(seq, rng, out);
+auto in = input_console_range();
+write(out, read(in));
 
 return 0;
 }
